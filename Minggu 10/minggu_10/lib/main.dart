a1c3aaf7a08f1a59d1148b67f90380b2ffcc6dde
SahrulGunawan10 <sg3533538@gmail.com>
2022-11-04 23:54:07 +0700

    project minggu 10

---
+++ Minggu 10/minggu_10/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:minggu_10/get_data.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Get API',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: pageOne(),
+      // getPages : pageRouteApp.pages
+      home: GetDataScreen(),
+    );
+  }
+}
