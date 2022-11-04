aea53133551054cf0a3ae74a41678b6206a7c09f
SahrulGunawan10 <sg3533538@gmail.com>
2022-11-04 23:51:02 +0700

    delete

--- Minggu 10/minggu_10_moviedb/android/app/src/main/kotlin/com/example/minggu_10_moviedb/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.minggu_10_moviedb
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Minggu 10/project_minggu_10/android/app/src/main/kotlin/com/example/project_minggu_10/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.project_minggu_10
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
